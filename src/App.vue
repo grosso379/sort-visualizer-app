44f1cc2e116bd68f9df4ced9d86efad1ddeb6195
grosso379 <49928776+grosso379@users.noreply.github.com>
2021-04-10 12:26:43 -0500

    Basic layout finished

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Lato", Helvetica, Arial, sans-serif;
 }
 </style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,58 +1,137 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="wrap">
+    <div class="navBar">
+      <h1 class="title">Sort Algorithms Visualizer</h1>
+      <div class="formsContainer">
+        <select v-model="selectedAlgorithm" class="form-select" aria-label="Default select example">
+          <option value selected>Choose a sort algorithm</option>
+          <option v-for="(algorithm, name) in algorithms" :key="name" :value="name" >
+            {{name}}
+          </option>
+        </select>
+        <select v-model="selectedSpeed" class="form-select" aria-label="Default select example">
+          <option value selected>Choose a speed</option>
+          <option v-for="speed in speeds" :key="speed" :value="speed" >
+            {{speed}}
+          </option>
+        </select>
+        <select v-model="selectedSize" class="form-select" aria-label="Default select example" @change="randomArray(selectedSize)">
+          <option value selected>Choose an array size</option>
+          <option v-for="size in sizes" :key="size" :value="size" >
+            {{size}}
+          </option>
+        </select>
+      </div>
+    </div>
+    <div class="arrayContainer">
+      <div v-for="num in array" :key="num" class="bar" :style="{height: (num / array.length) * 80 + '%'}"></div>
+    </div>
+    <div class="footer">
+      <h2>This is a footer</h2>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'MainPage',
+  data () {
+    return {
+      array: [],
+      algorithms: 
+        {
+        "Insertion Sort": this.instertionSort(this.array), 
+        "Selection Sort": this.selectionSort(this.array), 
+        "Bubble Sort": this.bubbleSort(this.array),
+      },
+      speeds:[1,2,3,4,5],
+      sizes:[100, 250, 500, 750, 1000],
+      selectedAlgorithm: "",
+      selectedSpeed:"",
+      selectedSize:"",
+    }
+  },
+  mounted() {
+    this.array = this.createArray(100)
+  },
+  methods : {
+    shuffle(array) {
+      array.sort(() => Math.random() - 0.5);
+    },
+
+    createArray(n) {
+      //Array of size n starting from 1 to n
+      return Array.from(new Array(n), (x,i) => i+1)
+    },
+
+    randomArray(n){
+      if (!Number.isInteger(n)){
+        n = 100
+      }
+      let a = this.createArray(n)
+      this.shuffle(a)
+      this.array = a
+    },
+
+    selectionSort(arr){
+      return arr
+    },
+
+    instertionSort(arr){
+      return arr
+    },
+
+    bubbleSort(arr){
+      return arr
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.wrap{
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  color: white;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.navBar{
+  background-color:#34495e;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.title {
+  font-size: 1.7rem;
+  padding: 20px 0px 10px;
 }
-a {
-  color: #42b983;
+
+.formsContainer{
+  display: flex;
+  flex-direction: row;
 }
+
+.form-select{
+  margin: 0px 20px 20px 10px;
+}
+
+.arrayContainer{
+  display: flex;
+  align-items: flex-end;
+  flex-direction: row;
+  flex-grow: 1;
+  width: 100%;
+  padding: 10px 25px;
+}
+
+.footer{
+  background-color: #34495e;
+}
+
+.bar{
+  flex-grow: 1;
+  background-color: lightblue;
+}
+
 </style>
